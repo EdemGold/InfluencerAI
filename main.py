3c5ba87121f86c4351d488cf4332bd9afdd696bd
dankornas <86244964+dankornas@users.noreply.github.com>
2021-11-01 21:33:00 +0100

    Added main class for starting app

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-"""
-
-File responsible for the application start
+from core.Core import Core
 
 """
+    Main class. Responsible for running the application.
+"""
+
+
+class Main:
+    @staticmethod
+    def run():
+        try:
+            app = Core.openController("home")
+            app.main()
+        except Exception as e:
+            print(str(e))
+
+
+if __name__ == '__main__':
+    Main.run()
