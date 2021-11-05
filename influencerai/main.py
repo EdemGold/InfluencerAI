6b8eb1ab07208cb8569e901b6b0fb353b39ee652
dankornas <86244964+dankornas@users.noreply.github.com>
2021-11-05 17:01:18 +0100

    Added demo pages

--- influencerai/main.py
+++ influencerai/main.py
@@ -1,13 +1,7 @@
 # from core.Core import Core
 import streamlit as st
 from postcreator.TwitterPostCreator import twitter_postcreator_view
-
-# """
-#     Main class. Responsible for running the application.
-# """
-
-
-        
+    
 
 
 if __name__ == '__main__':
@@ -18,7 +12,47 @@ if __name__ == '__main__':
     option = st.sidebar.selectbox("Which Dashboard?", (pages))
 
     if option == 'Google Search':
-        st.title("G")
+        st.title("<--- Choose Twitter from the sidebar!")
 
     elif option == 'Twitter':
         twitter_postcreator_view()
+
+    if option == "Instagram":
+        st.header("Instagram")
+        st.subheader("Instagram Logic Dashboard")
+        st.image('https://i.imgflip.com/5puwup.jpg')
+
+        st.write("""Hey there! Thanks for checking out this project! 
+        Now, this is just the beginning of what I have planned for the future. 
+        I really like python and I really like machine learning. 
+        So, putting these two things together, I think we can create 
+        a very interesting social media monitoring/content creation platform.""")
+
+        st.write("If you want to follow along and see how this projects unfold, be sure to follow wherever you can!")
+        st.write("Twitter:   https://twitter.com/dankornas")
+        st.write("Instagram: https://www.instagram.com/dankornas")
+        st.write("Facebook:  https://www.facebook.com/dankornas")
+
+        st.write("Special thanks a full credit for original content search and thread creation mechanism goes to Pratham be sure to follow him as well! (https://twitter.com/PrasoonPratham)")
+
+        st.image('https://clipground.com/images/instagram-logo-clear-background.png')
+
+    if option == "Facebook":
+        st.header("Facebook")
+        st.subheader("Facebook Logic Dashboard")
+        st.image('https://i.imgflip.com/5puwup.jpg')
+
+        st.write("""Hey there! Thanks for checking out this project! 
+        Now, this is just the beginning of what I have planned for the future. 
+        I really like python and I really like machine learning. 
+        So, putting these two things together, I think we can create 
+        a very interesting social media monitoring/content creation platform.""")
+
+        st.write("If you want to follow along and see how this projects unfold, be sure to follow wherever you can!")
+        st.write("Twitter:   https://twitter.com/dankornas")
+        st.write("Instagram: https://www.instagram.com/dankornas")
+        st.write("Facebook:  https://www.facebook.com/dankornas")
+
+        st.write("Special thanks a full credit for original content search and thread creation mechanism goes to Pratham be sure to follow him as well! (https://twitter.com/PrasoonPratham)")
+
+        st.image('http://www.amjazzy.it/wp-content/uploads/2017/08/facebook-logo-png-9.png')
